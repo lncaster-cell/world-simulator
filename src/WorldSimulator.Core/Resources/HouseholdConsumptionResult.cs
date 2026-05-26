7871d44eedeae1b2d0ffdb7e34688b4d3939e5c5
lncaster-cell <lncaster@bk.ru>
2026-05-26 17:37:49 +0300

    Restore full household consumption result model

--- src/WorldSimulator.Core/Resources/HouseholdConsumptionResult.cs
+++ src/WorldSimulator.Core/Resources/HouseholdConsumptionResult.cs
@@ -2,7 +2,21 @@ namespace WorldSimulator.Core.Resources;
 
 public sealed class HouseholdConsumptionResult
 {
+    public required int Population { get; init; }
+
+    public required decimal GoodsConsumptionPerPerson { get; init; }
+    public required decimal RequiredGoods { get; init; }
+    public required decimal GoodsAvailable { get; init; }
+    public required decimal GoodsConsumed { get; init; }
     public required decimal GoodsShortage { get; init; }
+
+    public required decimal ResourcesConsumptionPerPerson { get; init; }
+    public required decimal RequiredResources { get; init; }
+    public required decimal ResourcesAvailable { get; init; }
+    public required decimal ResourcesConsumed { get; init; }
     public required decimal ResourcesShortage { get; init; }
-    public required bool HasAnyShortage { get; init; }
+
+    public bool HasGoodsShortage => GoodsShortage > 0m;
+    public bool HasResourcesShortage => ResourcesShortage > 0m;
+    public bool HasAnyShortage => HasGoodsShortage || HasResourcesShortage;
 }
