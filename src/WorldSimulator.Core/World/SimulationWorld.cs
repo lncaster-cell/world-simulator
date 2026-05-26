a8431532300b413c120a6713fd27fb8ae13a4199
lncaster-cell <lncaster@bk.ru>
2026-05-26 19:30:37 +0300

    Add map coordinates for registry settlements

--- src/WorldSimulator.Core/World/SimulationWorld.cs
+++ src/WorldSimulator.Core/World/SimulationWorld.cs
@@ -5,6 +5,7 @@ namespace WorldSimulator.Core.World;
 public sealed class SimulationWorld
 {
     public required List<City> Cities { get; init; }
+    public required List<SettlementMapLocation> SettlementMapLocations { get; init; }
     public required string SelectedCityId { get; set; }
 
     public City SelectedCity => Cities.First(c => c.Id == SelectedCityId);
@@ -12,5 +13,10 @@ public sealed class SimulationWorld
     public City? FindCity(string id)
     {
         return Cities.FirstOrDefault(c => c.Id == id);
+    }
+
+    public SettlementMapLocation? FindSettlementMapLocation(string settlementId)
+    {
+        return SettlementMapLocations.FirstOrDefault(x => x.SettlementId == settlementId);
     }
 }
--- src/WorldSimulator.Core/World/WorldPresets.cs
+++ src/WorldSimulator.Core/World/WorldPresets.cs
@@ -20,6 +20,18 @@ public static class WorldPresets
                 CityPresets.CreateWodenz(),
                 CityPresets.CreateThokurRus()
             ],
+            SettlementMapLocations =
+            [
+                new SettlementMapLocation { SettlementId = "gotha", X = 0.6664m, Y = 0.2322m },
+                new SettlementMapLocation { SettlementId = "rivenstal", X = 0.4824m, Y = 0.4500m },
+                new SettlementMapLocation { SettlementId = "gavern", X = 0.5066m, Y = 0.5963m },
+                new SettlementMapLocation { SettlementId = "mlynek", X = 0.2833m, Y = 0.2487m },
+                new SettlementMapLocation { SettlementId = "brno", X = 0.4527m, Y = 0.7448m },
+                new SettlementMapLocation { SettlementId = "wodenz", X = 0.8036m, Y = 0.9604m },
+                new SettlementMapLocation { SettlementId = "wardmark", X = 0.0380m, Y = 0.4027m },
+                new SettlementMapLocation { SettlementId = "highrock", X = 0.1579m, Y = 0.2179m },
+                new SettlementMapLocation { SettlementId = "thokur_rus", X = 0.8652m, Y = 0.4753m }
+            ],
             SelectedCityId = "gotha"
         };
     }
