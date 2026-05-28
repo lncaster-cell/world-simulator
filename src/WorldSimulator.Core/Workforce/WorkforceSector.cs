74b0017f28da478137501d2d0f3cc093f6de637c
lncaster-cell <lncaster@bk.ru>
2026-05-28 22:26:46 +0300

    Add workforce sector enum

---
+++ src/WorldSimulator.Core/Workforce/WorkforceSector.cs
@@ -0,0 +1,14 @@
+namespace WorldSimulator.Core.Workforce;
+
+public enum WorkforceSector
+{
+    Agriculture,
+    Fishing,
+    Hunting,
+    ResourceGathering,
+    Crafting,
+    Trade,
+    Guards,
+    Maintenance,
+    Idle
+}
